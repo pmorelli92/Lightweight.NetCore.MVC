e45f9d6cd4fd06733275c3162ba9fa2691c450d0
Pablo Morelli <pmorelli@originsoftware.com.ar>
2017-08-30 11:39:30 -0300

    Updated to .net core 2.0

--- changelog.md
+++ changelog.md
@@ -1,5 +1,12 @@
 # Changelog
 
+### Version 2.0.0
+
+- Date 30/08/2017
+- Release notes:
+  - Updated to .NET Core 2.0
+  - Added HTTPS with dev certificate for Dev and Staging environments.
+  
 ### Version 1.0.0
 
 - Date 21/07/2017
--- src/Lightweight.NetCore.MVC.Presentation.Website/Startup.cs
+++ src/Lightweight.NetCore.MVC.Presentation.Website/Startup.cs
@@ -1,19 +1,37 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.IO.Compression;
 using WebMarkupMin.AspNet.Common.Compressors;
-using WebMarkupMin.AspNetCore1;
+using WebMarkupMin.AspNetCore2;
 
 namespace Lightweight.NetCore.MVC.Presentation.Website
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder =
+                new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("Properties/launchSettings.json", optional: false, reloadOnChange: true);
+
+            Environment = env;
+            Configuration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services
-                .AddWebMarkupMin()
+                .AddWebMarkupMin(opt => opt.AllowCompressionInDevelopmentEnvironment = true)
                 .AddXmlMinification()
                 .AddHtmlMinification()
                 .AddHttpCompression(options =>
@@ -22,6 +40,18 @@ namespace Lightweight.NetCore.MVC.Presentation.Website
                     }
                 );
 
+            // Development SSL
+            // TODO: Set logic for production since this will only use dev certificate
+            if (Environment.IsDevelopment() || Environment.IsStaging())
+            {
+                services.Configure<MvcOptions>(opt =>
+                {
+                    opt.Filters.Add(new RequireHttpsAttribute());
+                    opt.SslPort = Configuration.GetValue<int>("iisSettings:iisExpress:sslPort");
+                });
+            }
+            
+            services.AddAntiforgery();
             services.AddMvc();
         }
 
